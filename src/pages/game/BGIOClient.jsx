a779d59abb7139a1bb9f69fc081cd3207b9e51e2
Brian Tsao <btsao@us.ibm.com>
2020-05-20 20:12:55 -0700

    Attempt to fix constant reconnects on bgio

--- src/pages/game/BGIOClient.jsx
+++ src/pages/game/BGIOClient.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Client } from 'boardgame.io/react';
 import { SocketIO } from 'boardgame.io/multiplayer';
 
 export default function BGIOClient({ room, self, game, board }) {
     const hostname = window.location.hostname;
-    const GameClient = Client({
+    const [GameClient] = useState(Client({
         game,
         board,
         multiplayer: SocketIO({ server: `${hostname}:8000` })
-    });
+    }));
 
     const gameID = room.ctx.gameID;
     const { id, name, credentials } = room.state.players[self.id];

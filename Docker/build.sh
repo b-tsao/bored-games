6e371b8a6624806af80548f1b9b8985d51ef705c
BRIAN TSAO <btsao1790@gmail.com>
2022-05-06 17:56:03 -0700

    update build scripts for docker

--- Docker/build.sh
+++ Docker/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 cd .. && \
+./build.sh && \
 docker build -t bored-games --file Docker/Dockerfile .
--- Docker/update.sh
+++ Docker/update.sh
@@ -8,7 +8,7 @@ then
   export $(cat $env_file | sed 's/#.*//g' | xargs)
 fi
 
-./build.sh
-docker rm -vf $name
-docker run -d -p 3000:$REACT_APP_PORT -p 8000:$REACT_APP_BGIO_PORT --name $name bored-games
+./build.sh && \
+docker rm -vf $name && \
+docker run -d -p 3000:$REACT_APP_PORT -p 8000:$REACT_APP_BGIO_PORT --name $name bored-games && \
 docker image prune -f

f85330ae2d95af57e2aee4e09b0a1f22e75c62ac
BRIAN TSAO <btsao1790@gmail.com>
2022-05-16 18:01:19 -0700

    gdi

--- src/games/xp-werewolf/game/index.ts
+++ src/games/xp-werewolf/game/index.ts
@@ -44,7 +44,7 @@ export const XPWerewolf = {
                     players[pid] = {
                         ...others,
                         secret: '',
-                        vote: '?'
+                        vote: vote !== '' ? '?' : ''
                     };
                 }
             }

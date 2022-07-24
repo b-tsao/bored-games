404bb88ae592d679c198d003b524c624b2c0ab2a
BRIAN TSAO <btsao1790@gmail.com>
2022-07-24 12:46:28 -0700

    minor fix

--- src/pages/game/Settings.tsx
+++ src/pages/game/Settings.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import Maintenance from '../Maintenance';
 
 import { ChineseWerewolfSettings } from '../../games/chinese-werewolf/ui/Settings';
-import { XPWerewolfSettings } from '../../games/xp-werewolf/ui/Settings';
 
 export default function Settings({ room, self }) {
     switch (room.ctx.id) {
@@ -17,8 +16,8 @@ export default function Settings({ room, self }) {
         //     return <BGIOClient room={room} self={self} game={Mahjong} board={MahjongTable} />
         case 'chinese-werewolf':
             return <ChineseWerewolfSettings room={room} self={self} />
-        // case 'xp-werewolf':
-        //     return <XPWerewolfSettings room={room} self={self} />
+        // case 'reveal-werewolf':
+        //     return <RevealWerewolfSettings room={room} self={self} />
         default:
             return <Maintenance />
     }
